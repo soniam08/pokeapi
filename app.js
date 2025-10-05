1b03d13527ca4ab570bbe82f0c4cd840d9384e14
soniam08 <soniapdw@gmail.com>
2025-10-05 19:11:29 +0100

    Aclaraciones del código

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 async function getPokemon(id) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    const data = await response.json()
-    return data
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`) //Pide los datos de un pokemon especifico (id) a la pokeapi en internet
+    const data = await response.json() //cuando la funcion este lista crea un objeto (json) para trabajar con los datos en js
+    return data //devuelve los datos
 }
 
-console.log(getPokemon(175))
+console.log(getPokemon(175)) //muestra por consola la prome y posteriormente los datos
 
 
-// Crear el HTML de la tarjeta del Pokémon y añadirlo al contenedor
+// Generación del contenido HTML para la tarjeta
 function createCard(pokemon) {
-    const nombre = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-    const imagen = pokemon.sprites.front_default;
+    const nombre = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1); //La inicial en mayús y el resto en minuscula
+    const imagen = pokemon.sprites.front_default; //Extrae la imagen
     const altura = pokemon.height;
     const peso = pokemon.weight;
-    const tipos = pokemon.types.map(t => t.type.name).join(', ');
+    const tipos = pokemon.types.map(t => t.type.name).join(', '); //Map crea un vector de los tipos y los separa con el join , si fuera mas de uno
 
-    // Crea el HTML de la tarjeta
+    // Rellena un molde de HTML
     const cardHTML = `
         <div class="card">
             <img src="${imagen}" alt="${nombre}">
